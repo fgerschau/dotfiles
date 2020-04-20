d3a186f568d9d751d77dc5fcde4d08f6b8b7bd24
Felix Gerschau <felix.gerschau@gmail.com>
2020-04-20 11:51:41 +0200

    Add install scripts

--- install.sh
+++ install.sh
@@ -2,3 +2,27 @@
 defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 
+# brew install neovim \
+#             zsh \
+#             tmux \
+#             wget \
+#             nvm
+
+# Install zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo apt-get install zsh
+
+# change vim to nvim
+ln -s /usr/local/bin/nvim /usr/local/bin/vim
+
+# Load neovim configurations
+ln -s ~/dotfiles/.vim/init.vim ~/.config/nvim/init.vim
+
+# Create symlink to zshrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+
+# Create symlink to tmux config
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+# change zsh to default shell
+chsh -s $(which zsh)
